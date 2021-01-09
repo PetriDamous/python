dd4bc536cbf2fd102afb398e690c5c3465dd3e04
dwilliams64 <dwilliams_64@yahoo.com>
2021-01-09 17:07:43 -0600

    Started python 101, completed code hawke

--- code_hawk/test.py
+++ code_hawk/test.py
@@ -1,1 +1,19 @@
-print("it worked!!!")
+def str_search(term, search_str):
+    if term in search_str:
+        return "Term found"
+    else:
+        return "Term not found"
+
+string = "the dog ate the fox"
+
+result_1 = str_search("fox", string)
+
+result_2 = str_search("cat", string)
+
+print(result_1)
+
+print(result_2)
+
+# val = "fox" in "dog ate the fox"
+
+# print(val)
