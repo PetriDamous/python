2d4f5c4daf555bb59aafad25d44e2ec702b28c3b
dwilliams64 <dwilliams_64@yahoo.com>
2021-01-13 15:04:36 -0600

    added instuctor solution for input exercise, added new files for list exercise

--- python_101/exercises/13_user_input_ex/ans.py
+++ python_101/exercises/13_user_input_ex/ans.py
@@ -14,7 +14,11 @@ def convert(kilo):
 print(f"Hello {name}, you have travled {round(convert(miles), 1)} miles")
 
 
-
+#Instructor solution
+name = input('Enter your name: ')
+distance_km = input('Enter distance in km: ')
+distance_mi = float(distance_km)/1.609
+print(f'Hi {name.title()}! {distance_km}km is equivalent to {round(distance_mi,1)} miles.')
 
 
 
